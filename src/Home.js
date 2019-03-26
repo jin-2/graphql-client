fea7c4a4ffe23f433230bc76bfadeaaf3ba3100e
이수진 <jmakeaz@gmail.com>
2019-03-26 10:19:01 +0900

    #3 Setting up React Router(hashRouter)

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+// const Home = () => <div>Home</div>;
+
+class Home extends Component {
+    render() {
+        return <div>Home</div>;
+    }
+}
+
+export default Home;
