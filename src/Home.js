7f5c42058e01337f16bf2218d515de5ebc2d7869
이수진 <jmakeaz@gmail.com>
2019-04-02 10:00:00 +0900

    Getting data from the GraphQL API part One

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Query} from 'react-apollo';
+import {HOME_PAGE} from './queries';
 
-// const Home = () => <div>Home</div>;
+const Home = () => (
+    <Query query={HOME_PAGE}>
+        {({ loading, error, data }) => {
+            if (loading) return 'Loading...';
+            if (error) return `Error! ${error.message}`;
 
-class Home extends Component {
-    render() {
-        return <div>Home</div>;
-    }
-}
+            return (
+                <div>
+                    {data.movies.map(movie => (
+                        <article key={movie.id}>
+                            <h1>{movie.title}</h1>
+                            <p>{movie.rating}</p>
+                            <div>
+                                <img src={movie.medium_cover_image} alt="" />
+                            </div>
+                        </article>
+                    ))}
+                </div>
+            );
+        }}
+    </Query>
+);
 
 export default Home;
